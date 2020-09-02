8a7975a50f11783d4c14481292b64e1e9f20271f
Daniele Silvestro <silvestro.daniele@gmail.com>
2020-09-02 20:40:51 +0200

    added time variable q (linear)

--- rootBBB.py
+++ rootBBB.py
@@ -21,6 +21,7 @@ p.add_argument('-verbose',  type=int,   help='verbose', default = 1)
 p.add_argument('-sim',      type=int,   help='if >1 run simulations', default = 0)
 p.add_argument('-biased_q', type=int,   help='if 1 set increasing q through time', default = 0)
 p.add_argument('-freq_q0',  type=float, help='frequency of 0-sampling rate', default = 0.1)
+p.add_argument('-q_var',    type=int,   help='0) constant q 1) linearly changing q', default = 0)
 p.add_argument('-clades',   type=int,   help='range of clade to be analyzed', default = [0,0], nargs=2)
 p.add_argument('-outpath',  type=str,   help='path output', default = ".")
 p.add_argument('-clade_name', type=str,   help='', default = "")
@@ -45,7 +46,7 @@ if args.seed== -1:
 else:
 	seed =args.seed
 np.random.seed(seed)
-
+small_number = 10e-10
 
 n_simulations = args.sim
 save_mcmc_samples = 1
@@ -55,10 +56,12 @@ sim_loglinear = 0
 freq_par_updates = args.f
 verbose = args.verbose
 DAbatch = args.DAbatch
+q_var_model = args.q_var
 
 # simulation settings
 increasing_q_rates = args.biased_q
 freq_zero_preservation = args.freq_q0
+
 
 
 
@@ -91,8 +94,8 @@ def binomial_pmf(x,n,p):
 	#else: 
 	#	return approx_log_binomiam_pmf(x,n,p)
 
-def normal_pdf(x,n,p):
-	return scipy.stats.norm.logpdf(x, n, p, loc=0)
+def normal_pdf(x,m,s):
+	return scipy.stats.norm.logpdf(x, loc=m, scale=s)
 
 def gamma_pdf(x,a,b):
 	return scipy.stats.gamma.logpdf(x, a, scale=1./b)
@@ -169,7 +172,7 @@ def sample_path_batch_discrete(time_bins = np.arange(100), n_reps = 1, y_start =
 	B[:, -1] = 0 # added: set the last B to Zero
 	return B
 	
-def get_imputations(log_Nobs, Nobs, fossil_data, est_root, est_sig2, est_q, n_samples=1000):
+def get_imputations(log_Nobs, Nobs, fossil_data, est_root, est_sig2, n_samples=1000):
 	time_bins = mid_points[mid_points<est_root]
 	simTraj_all = np.zeros((n_samples,len(time_bins)))
 	j=0
@@ -207,11 +210,20 @@ def get_imputations(log_Nobs, Nobs, fossil_data, est_root, est_sig2, est_q, n_sa
 			return [], 0
 	return x_augmented, simTraj_all
 	
-def get_avg_likelihood(log_Nobs, Nobs, fossil_data, est_root, est_sig2, est_q, x_augmented, simTraj_all):
+def get_avg_likelihood(log_Nobs, Nobs, fossil_data, est_root, est_sig2, est_q, est_a, x_augmented, simTraj_all):
 	if len(x_augmented)==0:
 		return -np.inf, 0
 	else:
-		sampling_prob_vec = np.ones(len(x_augmented))- np.exp(-est_q)
+		dt = 1./age_oldest_obs_occ
+		time_vec = np.arange(len(x_augmented)) - age_oldest_obs_occ/2
+		time_vec *= dt
+		q_vec = est_q + (-est_a)*time_vec
+		q_vec[q_vec<0] = small_number
+		#print(q_vec)
+		
+		
+		sampling_prob_vec = np.ones(len(x_augmented))- np.exp(-q_vec)
+		# print(np.log(sampling_prob_vec))
 		if sim_loglinear:
 			lik_matrix = binomial_pmf(x_augmented,np.exp(simTraj_all),sampling_prob_vec)
 		else:
@@ -230,6 +242,8 @@ def run_mcmc(age_oldest_obs_occ, x, log_Nobs, Nobs, sim_n = 0):
 	lik_A = np.nan
 	tries = 0
 	while np.isnan(lik_A):
+		est_a_A = 0
+		
 		if tries <= 100:
 			# init root age
 			#est_root_A =  np.random.random()
@@ -241,14 +255,14 @@ def run_mcmc(age_oldest_obs_occ, x, log_Nobs, Nobs, sim_n = 0):
 				est_sig2_A = np.log(np.random.uniform(10, 50)*Nobs)
 			# init q_rate
 			est_q_A = np.random.uniform(0.0005, 0.002)
-			x_augmented_A, simTraj_all_A = get_imputations(log_Nobs, Nobs, x, est_root_A, est_sig2_A, est_q_A, n_samples=n_DA_samples)
+			x_augmented_A, simTraj_all_A = get_imputations(log_Nobs, Nobs, x, est_root_A, est_sig2_A, n_samples=n_DA_samples)
 			if len(x_augmented_A)==0:
 				# ie imputation didn't work
 				lik_A = np.nan
 			else:
-				lik_A, DA_counts =          get_avg_likelihood(log_Nobs, Nobs, x, est_root_A, est_sig2_A, est_q_A, x_augmented_A, simTraj_all_A)
+				lik_A, DA_counts =          get_avg_likelihood(log_Nobs, Nobs, x, est_root_A, est_sig2_A, est_q_A, est_a_A, x_augmented_A, simTraj_all_A)
 				#prior_A = gamma_pdf(est_sig2_A,a=1.,b=1.)
-				prior_A = gamma_pdf(np.exp(est_sig2_A-log_Nobs),a=1.,b=.1) + gamma_pdf(est_q_A,a=1.1,b=1)
+				prior_A = gamma_pdf(np.exp(est_sig2_A-log_Nobs),a=1.,b=.1) + gamma_pdf(est_q_A,a=1.1,b=1) + normal_pdf(est_a_A,0,1) 
 		elif tries <= 1000:
 			est_root_A =  age_oldest_obs_occ*(1+np.random.uniform(0.05,1 ))
 			# init sig2
@@ -259,30 +273,34 @@ def run_mcmc(age_oldest_obs_occ, x, log_Nobs, Nobs, sim_n = 0):
 			# init q_rate
 			est_q_A = np.random.uniform(0.0005, 0.002)
 
-			x_augmented_A, simTraj_all_A = get_imputations(log_Nobs, Nobs, x, est_root_A, est_sig2_A, est_q_A, n_samples=n_DA_samples)
+			x_augmented_A, simTraj_all_A = get_imputations(log_Nobs, Nobs, x, est_root_A, est_sig2_A, n_samples=n_DA_samples)
 			if len(x_augmented_A)==0:
 				# ie imputation didn't work
 				lik_A = np.nan
 			else:
-				lik_A, DA_counts =          get_avg_likelihood(log_Nobs, Nobs, x, est_root_A, est_sig2_A, est_q_A, x_augmented_A, simTraj_all_A)
+				lik_A, DA_counts =          get_avg_likelihood(log_Nobs, Nobs, x, est_root_A, est_sig2_A, est_q_A, est_a_A, x_augmented_A, simTraj_all_A)
 				#prior_A = gamma_pdf(est_sig2_A,a=1.,b=1.)
-				prior_A = gamma_pdf(np.exp(est_sig2_A-log_Nobs),a=1.,b=.1) + gamma_pdf(est_q_A,a=1.1,b=1)
+				prior_A = gamma_pdf(np.exp(est_sig2_A-log_Nobs),a=1.,b=.1) + gamma_pdf(est_q_A,a=1.1,b=1) + normal_pdf(est_a_A,0,1) 
 		else:
 			sys.exit("Failed to initialize model.")
 		tries+=1
 		
 	print("Running MCMC...")
 	if save_mcmc_samples:
+		if q_var_model:
+			model_out = "_qvar"
+		else:
+			model_out = ""
 		if run_simulations:
-			out_name = "%s/mcmc_%s_%s_f%s%s.log" % (args.outpath, sim_n, seed, freq_par_updates, args.out)
+			out_name = "%s/mcmc_%s_%s_f%s%s%s.log" % (args.outpath, sim_n, seed, freq_par_updates, args.out, model_out)
 			logfile = open(out_name, "w") 
-			text_str = "iteration\tposterior\tlikelihood\tprior\tNobs\tNfossils\troot_obs\troot_true\tq_med_true\tsig2_true\tDA_counts\troot_est\tq_est\tsig2_est"
+			text_str = "iteration\tposterior\tlikelihood\tprior\tNobs\tNfossils\troot_obs\troot_true\tq_med_true\tsig2_true\tDA_counts\troot_est\tq_est\ta_est\tsig2_est"
 			logfile.writelines(text_str)
 		else:
-			out_name = "%s/%s_mcmc_%s_f%s%s.log" % (args.outpath, sim_n, seed, freq_par_updates, args.out)
+			out_name = "%s/%s_mcmc_%s_f%s%s%s.log" % (args.outpath, sim_n, seed, freq_par_updates, args.out, model_out)
 			print(out_name)
 			logfile = open(out_name, "w") 
-			text_str = "iteration\tposterior\tlikelihood\tprior\tNobs\tNfossils\troot_obs\tDA_counts\troot_est\tq_est\tsig2_est"
+			text_str = "iteration\tposterior\tlikelihood\tprior\tNobs\tNfossils\troot_obs\tDA_counts\troot_est\tq_est\t\ta_est\tsig2_est"
 			logfile.writelines(text_str)
 			
 	res = np.zeros((int(args.n/args.s), 4))
@@ -292,7 +310,7 @@ def run_mcmc(age_oldest_obs_occ, x, log_Nobs, Nobs, sim_n = 0):
 	for iteration in range(args.n):
 		accepted = 0
 		#start = datetime.now()
-		est_root,est_sig2,est_q = est_root_A+0,est_sig2_A+0,est_q_A+0
+		est_root, est_sig2, est_q, est_a = est_root_A+0, est_sig2_A+0, est_q_A+0, est_a_A+0
 		x_augmented, simTraj_all = x_augmented_A+0, simTraj_all_A+0
 		h1,h2,h3 = 0,0,0
 		accept = 0
@@ -303,7 +321,7 @@ def run_mcmc(age_oldest_obs_occ, x, log_Nobs, Nobs, sim_n = 0):
 			accept = 0
 			
 		else: 
-			x_augmented, simTraj_all = get_imputations(log_Nobs, Nobs, x, est_root, np.exp(est_sig2), est_q, n_samples=n_DA_samples)
+			x_augmented, simTraj_all = get_imputations(log_Nobs, Nobs, x, est_root, np.exp(est_sig2), n_samples=n_DA_samples)
 			update = 0
 			accept = 1
 		
@@ -317,22 +335,25 @@ def run_mcmc(age_oldest_obs_occ, x, log_Nobs, Nobs, sim_n = 0):
 					est_sig2EXP, h2 = update_multiplier(np.exp(est_sig2_A),args.ws[1])
 					est_sig2 = np.log(est_sig2EXP)
 					est_q   , h3 = update_multiplier(est_q_A ,d=args.ws[2])
-			x_augmented, simTraj_all = get_imputations(log_Nobs, Nobs, x, est_root, np.exp(est_sig2), est_q, n_samples=n_DA_samples)
+					if q_var_model:
+						est_a, _ = update_normal(est_a_A, d=0.001)
+			x_augmented, simTraj_all = get_imputations(log_Nobs, Nobs, x, est_root, np.exp(est_sig2), n_samples=n_DA_samples)
 			accept = 0
 		
 		
 		
 		if sim_loglinear:
-			lik, DA_counts = get_avg_likelihood(log_Nobs, Nobs, x, est_root, est_sig2, est_q, x_augmented, simTraj_all )
+			lik, DA_counts = get_avg_likelihood(log_Nobs, Nobs, x, est_root, est_sig2, est_q, est_q, est_a, x_augmented, simTraj_all )
 		else:
-			lik, DA_counts = get_avg_likelihood(log_Nobs, Nobs, x, est_root, np.exp(est_sig2), est_q, x_augmented, simTraj_all )
+			lik, DA_counts = get_avg_likelihood(log_Nobs, Nobs, x, est_root, np.exp(est_sig2), est_q, est_a, x_augmented, simTraj_all )
 		
-		prior = gamma_pdf(np.exp(est_sig2-log_Nobs),a=1.,b=0.1) + gamma_pdf(est_q,a=1.1,b=1) 
+		prior = gamma_pdf(np.exp(est_sig2-log_Nobs),a=1.,b=0.1) + gamma_pdf(est_q,a=1.1,b=1) + normal_pdf(est_a,0,1) 
 	
 		if (lik-lik_A) + (prior-prior_A) + (h1+h2+h3) >= np.log(np.random.random()) or accept==1 and np.isfinite(lik):
 			est_root_A = est_root
 			est_sig2_A = est_sig2
 			est_q_A    = est_q
+			est_a_A    = est_a
 			lik_A      = lik
 			prior_A    = prior
 			x_augmented_A, simTraj_all_A = x_augmented, simTraj_all 
@@ -340,19 +361,19 @@ def run_mcmc(age_oldest_obs_occ, x, log_Nobs, Nobs, sim_n = 0):
 	
 		if iteration % args.p == 0 and verbose:
 			if iteration == 0:
-				print("%s\t%s\t%s\t%s\t%s" % ("it", "lik", "root", "sig2", "q_rate"))
-			print("%s\t%s\t%s\t%s\t%s" % (iteration, round(lik_A,2), round(est_root_A,2), round(est_sig2_A,2), round(est_q_A,5)))
+				print("%s\t%s\t%s\t%s\t%s\t%s" % ("it", "lik", "root", "sig2", "q_rate", "q_slope"))
+			print("%s\t%s\t%s\t%s\t%s\t%s" % (iteration, round(lik_A,2), round(est_root_A,2), round(est_sig2_A,2), round(est_q_A,5), round(est_a_A,5)))
 		
 		if iteration % args.s == 0:
 			if save_mcmc_samples:
 				if run_simulations:
-					text_str = "\n%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s" % \
+					text_str = "\n%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s" % \
 					( iteration, lik_A+prior_A, lik_A, prior_A, Nobs, np.sum(x), age_oldest_obs_occ, true_root, np.median(true_q), \
 					#true_sig2, DA_counts, age_oldest_obs_occ*(1+est_root_A), est_q_A, est_sig2_A)
-					true_sig2, DA_counts, est_root_A, est_q_A, est_sig2_A)
+					true_sig2, DA_counts, est_root_A, est_q_A, est_a_A, est_sig2_A)
 				else:
-					text_str = "\n%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s" % \
-					( iteration, lik_A+prior_A, lik_A, prior_A, Nobs, np.sum(x), age_oldest_obs_occ,DA_counts, est_root_A, est_q_A, est_sig2_A)
+					text_str = "\n%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s" % \
+					( iteration, lik_A+prior_A, lik_A, prior_A, Nobs, np.sum(x), age_oldest_obs_occ,DA_counts, est_root_A, est_q_A, est_a_A, est_sig2_A)
 				logfile.writelines(text_str)
 				logfile.flush()
 			
